f930f5a7be87578b18b1adc5c2fcfd4ad3c2d5f7
Daichi Aoki <fremie.off@gmail.com>
2025-08-25 13:21:31 +0900

    style: biome 2.2.0 にスキーマを修正

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,10 +1,8 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../ui/layout/Header.tsx'
-
 import type { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools as TanStackQueryLayout } from '@tanstack/react-query-devtools'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import Header from '../ui/layout/Header.tsx'
 
 interface MyRouterContext {
   queryClient: QueryClient
